6b124cf12aa9dd35fefd4105f8c25d464fee4713
ThatBaldDude4 <taylorv0004@gmail.com>
2025-12-19 13:04:13 -0600

    set up site

---
+++ script.js
@@ -0,0 +1,39 @@
+const promoValueEl = document.getElementById("promoAmount");
+const phoneValueEl = document.getElementById("fmv");
+const deviceCostEl = document.getElementById("deviceCost");
+const submitBtn = document.getElementById("calcBtn");
+
+const totalSavingsEl = document.getElementById("totalSavings");
+const monthlyCreditEl = document.getElementById("monthlyCredit");
+const monthlyBeforeEl = document.getElementById("monthlyBefore");
+const creditAppliedEl = document.getElementById("monthlyDiscount");
+const monthlyPayment = document.getElementById("monthlyAfter");
+
+const billCreditEl = document.getElementById("oneTimeCredit");
+const remainingPromoEl = document.getElementById("remainingPromo");
+
+function calculateData() {
+    const promoValue = promoValueEl.value;
+    const phoneValue = phoneValueEl.value;
+    const deviceCost = deviceCostEl.value;
+
+    const grossSavings = promoValue - phoneValue;
+    const monthlyBeforeDis = roundToPenny(deviceCost / 24);
+    const monthlyPromoDis = roundToPenny(grossSavings / 24);
+    const monthlyAfterDis = roundToPenny(monthlyBeforeDis - monthlyPromoDis);
+
+    totalSavingsEl.textContent = `$${grossSavings}`;
+    monthlyCreditEl.textContent = `$${monthlyPromoDis}`;
+    monthlyBeforeEl.textContent = `$${monthlyBeforeDis}`;
+    creditAppliedEl.textContent = `-$${monthlyPromoDis}`;
+    monthlyPayment.textContent = `$${monthlyAfterDis}`;
+
+    billCreditEl.textContent = `$${phoneValue}`;
+    remainingPromoEl.textContent = `$${grossSavings}`;
+}
+
+function roundToPenny(num) {
+    return Math.round(num * 100) / 100
+}
+
+submitBtn.addEventListener("click", calculateData)
